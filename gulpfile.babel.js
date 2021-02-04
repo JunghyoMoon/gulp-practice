ea5d82a95ce34047d3b6c67ebf7776479a2851e9
JungHyoMoon <dsinhome70@gmail.com>
2021-02-04 11:43:01 +0900

    gulp sass is done. need minify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import ws from "gulp-webserver";
 import image from "gulp-image";
 import sass from "gulp-sass";
 
+sass.compiler = require("node-sass");
+
 const routes = {
     pug: {
         watch: "src/**/*.pug",
@@ -15,6 +17,11 @@ const routes = {
     img: {
         src: "src/img/*",
         dest: "build/img",
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css",
     },
 };
 
@@ -29,15 +36,22 @@ const server = () =>
 const img = () =>
     gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const style = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.scss.watch, style);
     // 때때로 용량이 큰 이미지를 최적화할 경우 겁나게 오래 걸릴 수 있으니, watch 사용에 신경쓸 것
     gulp.watch(routes.img.src, img);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, style]);
 // gulp.series : 순차 실행
 // gulp.parallel : 동시 실행
 const live = gulp.parallel([server, watch]);
